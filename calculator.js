65d43f6c94681dfcb69caeb10894f7a3a14b047c
Mark Livolsi <mark.c.livolsi@gmail.com>
2019-02-13 16:13:09 -0500

    Initial commit.

---
+++ calculator.js
@@ -0,0 +1,14 @@
+const screen = document.querySelector('.screen');
+
+
+
+
+
+
+function init() {
+    document.querySelector('.calculator').addEventListener('click', function (event) {
+        // screen.innerText = event.target.innerText;
+    });
+}
+
+init();
